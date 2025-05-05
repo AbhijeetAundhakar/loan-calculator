d1434b15906fb5e951d4502fab684ea8d559a242
Abhijeet <abhiaundhakar@gmail.com>
2025-05-05 17:05:39 +0530

    Minor changes in vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: "/loan-calculator",
   css: {
     postcss: './postcss.config.cjs'
-  },
-  base: "/loan-calculator",
+  }
 })
