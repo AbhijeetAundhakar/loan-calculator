b9861b7a8004fbca96b1c205721328c088c5b9a0
Abhijeet <abhiaundhakar@gmail.com>
2025-05-05 23:03:46 +0530

    Removed code from vite.config.js file

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: 'index.html', // fallback page
-          dest: '.', // copy to root of dist
-          rename: '404.html' // GitHub Pages will serve this on 404
-        }
-      ]
-    })
-  ],
+  plugins: [react()],
   base: "/loan-calculator",
   css: {
     postcss: './postcss.config.cjs'
