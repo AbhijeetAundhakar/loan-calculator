562fc702459351e539b9be1b470bf1bc316ecbda
Abhijeet <abhiaundhakar@gmail.com>
2025-05-05 22:55:37 +0530

    Solved deployement issue

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'index.html', // fallback page
+          dest: '.', // copy to root of dist
+          rename: '404.html' // GitHub Pages will serve this on 404
+        }
+      ]
+    })
+  ],
   base: "/loan-calculator",
   css: {
     postcss: './postcss.config.cjs'
