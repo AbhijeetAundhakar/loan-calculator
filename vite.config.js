ba148b5aa5b3679816f41f777874bd650d51f696
Abhijeet <abhiaundhakar@gmail.com>
2025-05-05 23:08:27 +0530

    Added slash

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/loan-calculator",
+  base: "/loan-calculator/",
   css: {
     postcss: './postcss.config.cjs'
   }
